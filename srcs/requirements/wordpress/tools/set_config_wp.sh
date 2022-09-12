98eb88124930ecf1a2390d72dd78576bb55c11fd
ldermign <ldermign@student.42.fr>
2022-09-12 23:00:43 +0200

    end wordpress, make test, check file .yml

--- srcs/requirements/wordpress/tools/set_config_wp.sh
+++ srcs/requirements/wordpress/tools/set_config_wp.sh
@@ -1,7 +1,7 @@
 sleep 10
 
 wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_USER_PSWD --dbhost=mariadb:3306 --path='/var/www/wordpress'
-wp core install --url=$DOMAIN_NAME --title="Ldermign 42" --admin_user="ldermign" --admin_password="motdepasse_administrateur" --admin_email="ldermign@student.42.fr"
-wp user create dishdls
+wp core install --url=$DOMAIN_NAME --title="Ldermign 42" --admin_user=$MYSQL_ADMIN --admin_password=$MYSQL_ADMIN_PSWD --admin_email=$EMAIL
+wp user create $MYSQL_USER $EMAIL --role=administrator
 
 /usr/sbin/php-fpm7.3 -F
