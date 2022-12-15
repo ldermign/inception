b8f036e306a6784138418da3a56f43216d2a9837
Liena Dermigny <ldermign@student.42.fr>
2022-12-15 14:30:52 +0000

    check mariadb

--- srcs/requirements/mariadb/conf/start_mysql.sh
+++ srcs/requirements/mariadb/conf/start_mysql.sh
@@ -1,13 +1,18 @@
-mysql_install_db &> /dev/null
-service mysql start 2> /dev/null
+mysqld &
+# Rediriger la sortie d'erreur
+
+sleep 10
+# wait until mysqld is laucned
 
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_ADMIN}\`@'localhost' IDENTIFIED BY '${MYSQL_ADMIN_PSWD}';"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO \`${MYSQL_ADMIN}\`@'%' IDENTIFIED BY '${MYSQL_ADMIN_PSWD} WITH GRANT OPTION;"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO \`${MYSQL_ADMIN}\`@'%' IDENTIFIED BY '${MYSQL_ADMIN_PSWD}' WITH GRANT OPTION;"
 mysql -e "FLUSH PRIVILEGES;"
 
-mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
 
-killall mysqld
+mysqladmin -u root shutdown
+
+sleep 10
+# maybe a other while
+
 mysqld
-#exec mysqld_safe
