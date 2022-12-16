bff547d2793de3e40bb30103672bdcccfd55e1f2
Liena Dermigny <ldermign@student.42.fr>
2022-12-16 15:14:47 +0000

    not working in any way...

--- srcs/requirements/mariadb/conf/start_mysql.sh
+++ srcs/requirements/mariadb/conf/start_mysql.sh
@@ -1,18 +1,26 @@
+#!/bin/sh
+
 mysqld &
 # Rediriger la sortie d'erreur
 
-sleep 10
-# wait until mysqld is laucned
+while !(mysqladmin ping > /dev/null)
+    do
+        sleep 2
+    done
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_ADMIN}\`@'localhost' IDENTIFIED BY '${MYSQL_ADMIN_PSWD}';"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO \`${MYSQL_ADMIN}\`@'%' IDENTIFIED BY '${MYSQL_ADMIN_PSWD}' WITH GRANT OPTION;"
-mysql -e "FLUSH PRIVILEGES;"
+mysql -uroot -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+mysql -uroot -e "CREATE USER IF NOT EXISTS \`${MYSQL_ADMIN}\`@'localhost' IDENTIFIED BY '${MYSQL_ADMIN_PSWD}';"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO \`${MYSQL_ADMIN}\`@'%' IDENTIFIED BY '${MYSQL_ADMIN_PSWD}' WITH GRANT OPTION;"
+mysql -uroot -e "FLUSH PRIVILEGES;"
 
+# mysql -uroot -e "CREATE DATABASE IF NOT EXISTS \`InfoMySql\`;"
+# mysql -uroot -e "CREATE USER IF NOT EXISTS \`ldermign\`@'localhost' IDENTIFIED BY 'InceptionVM42';"
+# mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO \`ldermign\`@'%' IDENTIFIED BY 'InceptionVM42' WITH GRANT OPTION;"
+# mysql -uroot -e "FLUSH PRIVILEGES;"
 
 mysqladmin -u root shutdown
 
 sleep 10
-# maybe a other while
+# maybe an other while
 
 mysqld
--- srcs/requirements/wordpress/tools/set_config_wp.sh
+++ srcs/requirements/wordpress/tools/set_config_wp.sh
@@ -1,15 +1,15 @@
 sleep 10
 
-# wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_USER_PSWD --dbhost=mariadb:3306 --path=/var/www/wordpress
-# wp core install --allow-root --url=$DOMAIN_NAME --title=$TITLE --admin_user=$MYSQL_ADMIN --admin_password=$MYSQL_ADMIN_PSWD --admin_email=$EMAIL --path=/var/www/wordpress
-# wp user create --allow-root $MYSQL_USER --role=administrator
+wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_USER_PSWD --dbhost=mariadb:3306 --path=/var/www/wordpress
+wp core install --allow-root --url=$DOMAIN_NAME --title=$TITLE --admin_user=$MYSQL_ADMIN --admin_password=$MYSQL_ADMIN_PSWD --admin_email=$EMAIL --path=/var/www/wordpress
+wp user create --allow-root $MYSQL_USER --role=administrator
 
-wp config create	--allow-root \
-	--dbname=$MYSQL_DATABASE \
-    --dbuser=$MYSQL_USER \
-	--dbpass=$MYSQL_PASSWORD \
-	--dbhost=mariadb:3306 --path='/var/www/wordpress'
-wp core install --url=$DOMAIN_NAME --title=$TITLE --admin_user=$MYSQL_ADMIN --admin_password=$MYSQL_ADMIN_PSWD --admin_email=$EMAIL --path=/var/www/wordpress --allow-root --skip-email;
+# wp config create	--allow-root \
+# 	--dbname=$MYSQL_DATABASE \
+#     --dbuser=$MYSQL_USER \
+# 	--dbpass=$MYSQL_PASSWORD \
+# 	--dbhost=mariadb:3306 --path='/var/www/wordpress'
+# wp core install --url=$DOMAIN_NAME --title=$TITLE --admin_user=$MYSQL_ADMIN --admin_password=$MYSQL_ADMIN_PSWD --admin_email=$EMAIL --path=/var/www/wordpress --allow-root --skip-email;
 
 mkdir -p /run/php
 
